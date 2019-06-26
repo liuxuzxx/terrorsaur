fb0e6dbcde53003b239329253af093ba04a1b47c
lxzxx <928525166@qq.com>
2019-06-26 21:56:16 +0800

    新增var关键字的使用

--- src/main/terrorsaur.go
+++ src/main/terrorsaur.go
@@ -11,6 +11,21 @@ func main() {
 	fmt.Println(first, second, third)
 	fmt.Println("查看获取到的数据信息:", math.Pi)
 	fmt.Println(split(17))
+	fmt.Println(sum(34, 89))
+	fmt.Println(sumString("左手", "右手"))
+
+	logVarInformation()
+}
+
+/**
+ * Go的函数奇怪啊，这种类型放在名字后面有点美国人的味道
+ */
+func sum(left int, right int) int {
+	return left + right
+}
+
+func sumString(left, right string) string {
+	return left + right
 }
 
 func swap(left, right string) (string, string, string) {
@@ -21,4 +36,16 @@ func split(sum int) (x, y int) {
 	x = sum * 4 / 9
 	y = sum - x
 	return
+}
+
+/**
+实验var关键字
+感觉这个var就是声明一下，我这一行是变量variables
+总体的方式就是: var 变量名字 变量类型 = 初始化的数值
+*/
+var language, c, cpp, python, java, golang = "language", "C", "C++", "Python", "Java", "Golang"
+
+func logVarInformation() {
+	var count, numberStr = 1, "Yes"
+	fmt.Println(count, language, c, cpp, python, java, golang, numberStr)
 }
