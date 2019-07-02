6b4e7f1ad3189203930051f7542f15205f7a9f12
liuxu <18270719298@163.com>
2019-07-02 14:40:18 +0800

    安装了Go的一个web框架:iris 加入了上传文件的测试

--- src/main/terrorsaur.go
+++ src/main/terrorsaur.go
@@ -2,210 +2,72 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"runtime"
-	"time"
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/core/router"
+	"github.com/kataras/iris/middleware/logger"
+	recover2 "github.com/kataras/iris/middleware/recover"
+	"mime/multipart"
+	"strings"
 )
+
+const maxSize = 5 << 20
 
 func main() {
-	fmt.Println("尝试使用一下Idea进行Go语言的开发")
-	first, second, third := swap("liuxu", "zhongxiaoxia")
-	fmt.Println(first, second, third)
-	fmt.Println("查看获取到的数据信息:", math.Pi)
-	fmt.Println(split(17))
-	fmt.Println(sum(34, 89))
-	fmt.Println(sumString("左手", "右手"))
+	fmt.Println("直接开启Iris的Go的web编程")
+	app := iris.New()
+	app.Use(recover2.New())
+	app.Use(logger.New())
 
-	logVarInformation()
-	replaceVar()
-	showBasicTypes()
-	convertType()
-	equalDifferenceSeries(100)
-	binaryInt(20000)
-	squrt(-100)
-	deferTime()
-	checkSystemVersion()
-	pointer()
-	vertexInformation()
-	arrayInit()
-	sliceArray()
+	app.PartyFunc("/api/system", func(system router.Party) {
+		system.Get("/information", systemInformationHandler)
+		system.Get("/name", systemNameHandler)
+	})
+
+	app.PartyFunc("/api/user", func(users router.Party) {
+		users.Get("/information", userInformationHandler)
+		users.Get("/{userId:int}/status", userStatusHandler)
+		users.Get("/{userId:int}/birthday", userBirthdayHandler)
+	})
+
+	app.PartyFunc("/api/video", func(video router.Party) {
+		video.Post("/upload", iris.LimitRequestBodySize(maxSize), uploadVideoHandler)
+	})
+
+	app.Run(iris.Addr(":12309"), iris.WithConfiguration(iris.Configuration{
+		Charset: "UTF-8",
+	}))
 }
 
-/**
- * Go的函数奇怪啊，这种类型放在名字后面有点美国人的味道
- */
-func sum(left int, right int) int {
-	return left + right
+func systemInformationHandler(ctx iris.Context) {
+	ctx.JSON(iris.Map{"systemName": "Go-Iris系统", "status": "OK"})
 }
 
-func sumString(left, right string) string {
-	return left + right
+func systemNameHandler(ctx iris.Context) {
+	ctx.JSON(iris.Map{"systemName": "Go-Iris系统"})
 }
 
-func swap(left, right string) (string, string, string) {
-	return right, left, left + right
+func userInformationHandler(ctx iris.Context) {
+	ctx.JSON(iris.Map{"userName": "rootAdmin", "age": 20, "birthday": "1990-10-07"})
 }
 
-func split(sum int) (x, y int) {
-	x = sum * 4 / 9
-	y = sum - x
-	return
+func userStatusHandler(ctx iris.Context) {
+	userId := ctx.Params().Get("userId")
+	ctx.JSON(iris.Map{"status": "OK", "userId": userId})
 }
 
-/**
-实验var关键字
-感觉这个var就是声明一下，我这一行是变量variables
-总体的方式就是: var 变量名字 变量类型 = 初始化的数值
-*/
-var language, c, cpp, python, java, golang = "language", "C", "C++", "Python", "Java", "Golang"
-
-func logVarInformation() {
-	var count, numberStr = 1, "Yes"
-	fmt.Println(count, language, c, cpp, python, java, golang, numberStr)
+func userBirthdayHandler(ctx iris.Context) {
+	userId := ctx.Params().Get("userId")
+	ctx.JSON(iris.Map{"userId": userId, "birthday": "1990-10-06"})
 }
 
-/**
-:= 运算符号的使用说明 说白了就是替换var
-但是在函数外面不能使用
-使用这个可以推断inffered 类型
-*/
-func replaceVar() {
-	userName, password := "root", "root123"
-	fmt.Println(userName, password)
+func uploadVideoHandler(ctx iris.Context) {
+	ctx.UploadFormFiles("F:/", beforeSave)
 }
 
-/**
-Golang的变量系列
-*/
-var (
-	flag      bool   = true
-	cluesName string = "线索的名字"
-	count     uint64 = 1<<64 - 1
-)
+func beforeSave(ctx iris.Context, file *multipart.FileHeader) {
+	ip := ctx.RemoteAddr()
+	ip = strings.Replace(ip, ".", "_", -1)
+	ip = strings.Replace(ip, ":", "_", -1)
 
-func showBasicTypes() {
-	fmt.Println(flag, cluesName, count)
-}
-
-func convertType() {
-	const goLangVersion = "1.12.6"
-	driverClassName, connectionCount := "com.mysql.jdbc.Driver", 60
-	fmt.Print(driverClassName, float64(connectionCount), "Golang的版本号:", goLangVersion)
-}
-
-/**
-感觉GO的所有只要是你想声明多个的就使用括号就行了
-*/
-const (
-	driverClassName = "com.mysql.jdbc.Driver"
-	url             = "jdbc:mysql://localhost:3306"
-	port            = 3306
-	userName        = "root"
-	password        = "root124"
-)
-
-/**
-进入Go的控制类型的语句试验场
-发现Go声明变量怪怪的
-要么就是
-var 变量名 类型 = 初始化的数据
-要么就是 变量名 := 初始化的数据
-*/
-func equalDifferenceSeries(maxNumber int) {
-	sexNumber := 90
-	var sum = 0
-	for index := 0; index < maxNumber; index++ {
-		sum += index
-	}
-	fmt.Println(maxNumber, "等差数列的和是:", sum, sexNumber)
-}
-
-func binaryInt(maxNumber int) {
-	sum := 1
-	for sum < maxNumber {
-		sum += sum
-	}
-	fmt.Println("获取到的最大二进制:{}", sum)
-}
-
-func squrt(number float64) {
-	if number < 0 {
-		fmt.Println("小于0的数字没法操作")
-	} else {
-		fmt.Println("开平发的数字是:", math.Sqrt(number))
-	}
-}
-
-func checkSystemVersion() {
-	fmt.Print("Go语言运行的操作系统环境")
-	switch os := runtime.GOOS; os {
-	case "linux":
-		fmt.Println("Linux操作系统")
-	case "windows":
-		fmt.Println("Windows操作系统:", os)
-	default:
-		fmt.Print("不知道啥子系统:", os)
-	}
-
-	fmt.Println(time.Thursday)
-}
-
-/**
-defer这个关键字怎么看怎么像 try{函数所有的代码}finally{defer语句}
-*/
-func deferTime() {
-	defer fmt.Println("推迟执行")
-	fmt.Println("在这之前执行")
-}
-
-/**
-坏了，Go越看越像c了，简直就是一个翻版的c啊，指针来了，其他还会远吗
-看看指针
-*/
-
-func pointer() {
-	number := 90
-	numberPoint := &number
-
-	fmt.Println(*numberPoint)
-}
-
-type Vertex struct {
-	length int
-	width  int
-	height int
-}
-
-func vertexInformation() {
-	fmt.Print(Vertex{10, 90, 89})
-}
-
-/**
-初始化数组
-当使用 := 初始化的时候，复杂类型还是需要指定
-*/
-type UserDto struct {
-	userId    int64
-	userName  string
-	accountId string
-	password  string
-	birthday  string
-}
-
-func arrayInit() {
-	userArray := [1]UserDto{{1, "liuxu", "root", "root123", "2019-10-1"}}
-	fmt.Println(userArray)
-}
-
-/**
-切片数组，其实和Python一个味道
-slice就是一个裁剪的过程，说白了，就是裁剪的数据的数组
-保存着一个开始指针的引用和一个长度性质，我猜测是这么回事
-*/
-func sliceArray() {
-	userIds := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	sliceUserIds := userIds[2:6]
-	fmt.Println(sliceUserIds)
-	sliceUserIds[2] = 90
-	fmt.Println(userIds)
+	file.Filename = ip + "-" + file.Filename
 }
