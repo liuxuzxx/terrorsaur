6776b770ce0e7c577cd17d5906455d817a9fd095
liuxu <18270719298@163.com>
2019-07-29 17:19:39 +0800

    增加Config，并且读取配置到配置的struct当中去

--- main.go
+++ main.go
@@ -1,8 +1,49 @@
 package main
 
 import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
 )
 
-func main(){
+func main() {
+	fmt.Println("Terrorsaur Server启动了......")
+	fmt.Println(config.Server.Name)
+	fmt.Println(config.Server.Port)
+	fmt.Println(config.DataSource)
+}
 
+var config Config
+
+func init() {
+	fmt.Println("Start init the web config information!")
+	viper.AddConfigPath("./config")
+	viper.SetConfigName("config")
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("修改了配置文件")
+	})
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Print("解析出现错误")
+	}
+	err = viper.Unmarshal(&config)
+}
+
+type Config struct{
+	Server Server
+	DataSource DataSource
+}
+
+type Server struct{
+	Port int
+	Name string
+}
+
+type DataSource struct{
+	Ip string
+	Port int
+	UserName string
+	Password string
+	DatabaseName string
 }
