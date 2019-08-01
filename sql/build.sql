0079005afccb4cb7fb8e3977ad5b8fccb72880a6
liuxu <18270719298@163.com>
2019-08-01 17:33:35 +0800

    增加建表语句，下一步准备爬虫，爬取基本内容信息

--- sql/build.sql
+++ sql/build.sql
@@ -15,3 +15,24 @@ VALUES ('诗经','汉代、两晋及南北朝的乐府诗歌',1),
        ('元曲','格调的有一次提升，电视剧的前身',6),
        ('明清','封建社会进入了风烛残年的时代',7);
 
+
+drop table if exists author;
+create table author(
+    author_id int not null auto_increment primary key comment '作家的ID',
+    name char(10) not null comment '作家名字',
+    dynasty char(10) not null comment '朝代',
+    detail varchar(4000) not null comment '作家的详细信息'
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 comment '作家的信息';
+
+drop table if exists article;
+create table article(
+    article_id int not null auto_increment primary key comment '文章自增的ID',
+    author_id int not null comment '作家的ID',
+    title varchar(200) not null comment '文章题目',
+    content text(20000) not null comment '文章内容',
+    comment text(20000) not null comment '注解',
+    create_by varchar(100) not null comment '创建者',
+    create_time datetime not null default now() comment '创建时间',
+    update_by varchar(100) not null comment '修改者',
+    update_time datetime not null default now() comment '修改时间'
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 comment '古文信息' ;
