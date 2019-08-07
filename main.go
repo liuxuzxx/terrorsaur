730534864adb04d77ebdc5ba387f08ffef1273b9
liuxu <18270719298@163.com>
2019-08-07 17:39:39 +0800

    1、增加swagger文档生成的支持 2、不过这个swagger不能自动生成，还是需要按照swagger的语法来写才可以

--- main.go
+++ main.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"fmt"
+	"github.com/iris-contrib/swagger"
+	"github.com/iris-contrib/swagger/swaggerFiles"
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/core/router"
 	"github.com/kataras/iris/middleware/logger"
 	"github.com/kataras/iris/middleware/recover"
 	"log"
 	"strconv"
+	_ "terrorsaur/docs"
 	"terrorsaur/libs"
 	"terrorsaur/middleware"
 	"terrorsaur/rest"
 )
 
+// @title Terrorsaur-鸵鸟勇士
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host 172.16.21.207:12309
+// @BasePath /v1
 func main() {
 	fmt.Println("Terrorsaur Server is starting...")
 
@@ -29,6 +46,10 @@ func main() {
 
 func route() (app *iris.Application) {
 	app = iris.New()
+	config := &swagger.Config{
+		URL: "http://" + libs.Conf.Server.Domain + ":" + strconv.Itoa(libs.Conf.Server.Port) + "/swagger/doc.json", //The url pointing to API definition
+	}
+	app.Get("/swagger/{any:path}", swagger.CustomWrapHandler(config, swaggerFiles.Handler))
 
 	v1 := app.Party("/v1", middleware.LogRequestInformationHandler).AllowMethods(iris.MethodOptions)
 	{
--- rest/author_rest.go
+++ rest/author_rest.go
@@ -6,6 +6,12 @@ import (
 	"terrorsaur/service"
 )
 
+// 获取作者信息列表
+// @Summary Author信息列表
+// @Description 获取Author信息列表
+// @Accept  json
+// @Produce  json
+// @Router /api/rattrap/ancient-article/author/{authorId:int}/information [get]
 func AuthorInformation(context iris.Context) {
 	authorId, _ := context.Params().GetInt("authorId")
 	_, _ = context.JSON(common.Success(service.FetchAuthorByAuthorId(authorId)))
