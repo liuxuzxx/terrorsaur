b8a85c69ec2e3a92229fb2fdbee7fd3f161023a3
liuxu <928525166@qq.com>
2019-12-09 19:36:19 +0800

    提交字典和字典尔雅的数据信息

--- sql/dictionary.sql
+++ sql/dictionary.sql
@@ -11,3 +11,11 @@ create table dictionary_type
 insert into dictionary_type(dictionary_code, dictionary_name) VALUES ('DICT_ERYA','尔雅');
 insert into dictionary_type(dictionary_code, dictionary_name) VALUES ('DICT_SWJZ','说文解字');
 insert into dictionary_type(dictionary_code, dictionary_name) VALUES ('DICT_KXZD','康熙字典');
+
+drop table if exists dictionary;
+create table dictionary(
+    id int not null auto_increment primary key comment '主键自增id',
+    chinese_character varchar(20) not null comment '汉字',
+    explanation varchar(1000) not null comment '汉字解释',
+    dictionary_type int not null comment '词典类型，来源于哪一种词典'
+);
